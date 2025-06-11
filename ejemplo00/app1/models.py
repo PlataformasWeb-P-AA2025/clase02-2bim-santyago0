afb94390e812a1875b57e0e035d9350f48d8d0fd
santyago0 <santyago0515@gmail.com>
2025-06-11 08:31:59 -0500

    Crear clases y actualizar tablas

--- ejemplo00/app1/models.py
+++ ejemplo00/app1/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Estudiante(models.Model):
+    nombre = models.CharField(max_length=30)
+    apellido = models.CharField(max_length=30)
+    cedula = models.CharField(max_length=30, unique=True)
+    edad = models.IntegerField()
+
+    def __str__(self):
+        return "%s - %s - %s - edad: %d" % (self.nombre,
+                self.apellido,
+                self.cedula,
+                self.edad)
